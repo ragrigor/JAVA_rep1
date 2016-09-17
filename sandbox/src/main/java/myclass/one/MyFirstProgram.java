f8526b89a8e6212c4134dfc1ae9f2df74082e1d8
ragrigor <topemberley@gmail.com>
2016-09-17 19:21:32 +0300

    Task2 #4 done; constructor Point, distance method added

--- sandbox/src/main/java/myclass/one/MyFirstProgram.java
+++ sandbox/src/main/java/myclass/one/MyFirstProgram.java
@@ -2,23 +2,19 @@ package myclass.one;
 
 public class MyFirstProgram{
 	public static void main (String[] args) {
-		
 
-		Point p1 = new Point();
-		p1.x = 2;
-		p1.y = 3;
 
-		Point p2 = new Point();
-		p2.x = 5;
-		p2.y = 10;
-		double d = distance(p1, p2);
-		System.out.println(d);
-	}
+		Point p1 = new Point(2,3);
 
-	public static double distance(Point p1, Point p2){
-		return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+		Point p2 = new Point(5,10);
+        System.out.println(p1.distance (p1, p2));
+
+        //double d = distance(p1, p2);
+		//System.out.println(d);
+
 
 	}
+
 }
 
 
--- sandbox/src/main/java/myclass/one/Point.java
+++ sandbox/src/main/java/myclass/one/Point.java
@@ -4,4 +4,18 @@ package myclass.one;
 public class Point {
     public double x;
     public double y;
+
+
+
+
+    public Point(double x1, double y1) {
+        x = x1;
+        y = y1;
+
+    }
+    public double distance(Point p1, Point p2){
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+
+    }
+
 }
