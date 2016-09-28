380a2f80e31b69a8a3c30ab4c156623b0fe8fff7
ragrigor <topemberley@gmail.com>
2016-09-28 23:32:26 +0300

    goToGroupPage() , goToContactPage()  optimized

--- addressbook-web-test1/src/test/java/ru/web/adressbook/appmanager/HelperBase.java
+++ addressbook-web-test1/src/test/java/ru/web/adressbook/appmanager/HelperBase.java
@@ -2,6 +2,7 @@ package ru.web.adressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -36,5 +37,16 @@ public class HelperBase {
         } catch (NoAlertPresentException e) {
             return false;
         }
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            wd.findElement(locator);
+            return true;
+        }catch (NoSuchElementException ex){
+            return false;
+
+        }
+
     }
 }
--- addressbook-web-test1/src/test/java/ru/web/adressbook/appmanager/NavigationHelper.java
+++ addressbook-web-test1/src/test/java/ru/web/adressbook/appmanager/NavigationHelper.java
@@ -1,6 +1,7 @@
 package ru.web.adressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -15,10 +16,21 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))){
+        return;
+        }
         click(By.linkText("groups"));
     }
 
+
     public void gotoContactPage(){
+        if(isElementPresent(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home"));
     }
+
 }
+
