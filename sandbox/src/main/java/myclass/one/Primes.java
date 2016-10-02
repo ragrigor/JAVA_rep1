2a3f5116cb4b22f2b2935ff0a63e5433e72a7df7
ragrigor <topemberley@gmail.com>
2016-10-02 13:47:36 +0300

    Prime tests added

---
+++ sandbox/src/main/java/myclass/one/Primes.java
@@ -0,0 +1,33 @@
+package myclass.one;
+
+public class Primes {
+
+    public static boolean isPrime(int n){
+
+        for(int i = 2; i < n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPrimeFast(int n){
+        int m = (int) Math.sqrt(n);
+        for(int i = 2; i < m; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPrimeWhile(long n){
+         long i = 2;
+        while(i < n && n % i !=0) {
+            i ++;
+        }
+        return i == n ;
+    }
+}
+
