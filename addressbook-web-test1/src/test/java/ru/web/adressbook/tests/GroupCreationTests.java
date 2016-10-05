1596146dd89f5a53518c62058f3ff0b1ba61ff8f
ragrigor <topemberley@gmail.com>
2016-10-05 22:02:10 +0300

    Lists fixed, GroupCreationTests with lambda
    -

--- addressbook-web-test1/src/test/java/ru/web/adressbook/tests/GroupCreationTests.java
+++ addressbook-web-test1/src/test/java/ru/web/adressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.web.adressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,18 +15,19 @@ public class GroupCreationTests extends TestBase {
 
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("group5", "test2", "test22");
+        GroupData group = new GroupData("group16", "test2", "test22");
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() +1);
 
-        int max = 0;
+      /*  int max = 0;
         for(GroupData g : after){
             if (g.getId() > max){
                 max = g.getId();
             }
-        }
-        group.setID(max);
+        } */
+
+        group.setID(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
