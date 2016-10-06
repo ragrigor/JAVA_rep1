ae6bb843ba2fca36a3df09f48e09b4b15593d999
ragrigor <topemberley@gmail.com>
2016-10-06 21:25:15 +0300

    ContactCreAtionTests with Lambda
    max contact id

--- addressbook-web-test1/src/test/java/ru/web/adressbook/tests/ContactCreationTests.java
+++ addressbook-web-test1/src/test/java/ru/web/adressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.web.adressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,20 +15,21 @@ public class ContactCreationTests extends TestBase {
     public void testContactCreation() {
         app.getNavigationHelper().gotoContactPage();
         List<ContactData> before = app.getContactHelper().getContactList();
-        ContactData contact = new ContactData("Test1", null, "2 TestStreet ", "test22@gmail.com", null, "456-444", "456-555", "456-666");
+        ContactData contact = new ContactData("Test4", null, "2 TestStreet ", "test22@gmail.com", null, "456-444", "456-555", "456-666");
         app.getContactHelper().createContact(contact);
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
 
 
-        int max = 0;
+     /*   int max = 0;
         for (ContactData c : after){
             if(c.getId() > max) {
                 max = c.getId();
             }
-        }
-        contact.setId(max);
+        }  */
+
+        contact.setId(after.stream().max((c1, c2) -> Integer.compare(c1.getId(), c2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
