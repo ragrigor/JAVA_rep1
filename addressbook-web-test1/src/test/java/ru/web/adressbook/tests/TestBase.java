ca858a637339b576a869c13de0da992d0174124f
ragrigor <topemberley@gmail.com>
2016-10-24 23:31:02 +0300

    logback added

--- addressbook-web-test1/src/test/java/ru/web/adressbook/tests/TestBase.java
+++ addressbook-web-test1/src/test/java/ru/web/adressbook/tests/TestBase.java
@@ -1,12 +1,19 @@
 package ru.web.adressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.web.adressbook.appmanager.ApplicationManager;
 
-public class TestBase {
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
+public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -15,9 +22,19 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p){
+        logger.info("Start test "  + m.getName()+ " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m){
+        logger.info("Stop test "  + m.getName());
     }
 
 }
